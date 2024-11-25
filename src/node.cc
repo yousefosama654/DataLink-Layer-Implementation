515e5335c8df5014bba49a9e8939363a8d4851e0
nesma-shafie <nesmashafie342@gmail.com>
2024-11-25 23:53:06 +0200

    sending messages with errors

--- src/node.cc
+++ src/node.cc
@@ -14,17 +14,55 @@
 //
 
 #include "node.h"
-
+#define GetCurrentDir _getcwd
 Define_Module(Node);
+
+double delayTime = 0;
 
 void Node::initialize()
 {
     // TODO - Generated method body
+    windowStart = 0;
+    windowEnd = par("WindowSize").intValue();
+    messageIndex = 0;
+    maxSeqNo = par("WindowSize").intValue() * 2;
+
+    EV << par("TransmissionDelay").doubleValue() << "\n";
+    EV << par("ProcessingDelay").doubleValue() << "\n";
 }
 void Node::handleMessage(cMessage *msg)
 {
-    double paramValue = par("DuplicationDelay");
-    EV << "hello from " << this->getName() << " value is " << paramValue << "\n";
+    // double paramValue = par("DuplicationDelay");
+    // EV << "hello from " << this->getName() << " value is " << paramValue << "\n";
+    // EV << "hello from " << this->getName();
+
+    // print the recived message
+    EV << check_and_cast<Message_Base *>(msg)->getPayload() << endl;
+    Message_Base *mptr = check_and_cast<Message_Base *>(msg);
+
+    // if I recieve message from coordinator read the input file
+    if (std::string(mptr->getPayload()).rfind("input", 0) == 0)
+    {
+        std::string fileName = mptr->getPayload();
+        readInputFile(fileName, &errors, &messages);
+        isSender = true;
+    }
+
+    if (isSender)
+    {
+        delayTime = 0;
+        while (messageIndex < windowEnd && messageIndex < messages.size())
+        {
+            delayTime += par("ProcessingDelay").doubleValue();
+            Message_Base *new_msg = new Message_Base();
+            new_msg->setPayload(messages[messageIndex].c_str());
+            new_msg->setType(0);
+            new_msg->setHeader(messageIndex % maxSeqNo);
+            new_msg->setTrailer(0);
+            sendMessage(new_msg, errors[messageIndex], "out");
+            messageIndex++;
+        }
+    }
 }
 
 char Node::calculateParity(std::string &payload)
@@ -37,6 +75,7 @@ char Node::calculateParity(std::string &payload)
     }
     return parityByte;
 }
+
 void Node::framing(Message_Base *mptr, std::string &payload)
 {
     // TODO - Generated method body
@@ -60,11 +99,13 @@ void Node::framing(Message_Base *mptr, std::string &payload)
     mptr->setTrailer(parity);
     mptr->setType(0);
 }
-void Node::modifyMessage(std::string &payload)
+void Node::modifyMessage(Message_Base *msg)
 {
+    std::string payload = msg->getPayload();
     int bitIdx = rand() % 8;
     int byteIdx = rand() % payload.length();
     payload[byteIdx] ^= (1 << bitIdx);
+    msg->setPayload(payload.c_str());
 }
 void Node::openOutputFile()
 {
@@ -94,4 +135,66 @@ void Node::Timeout_print(int seqnum)
     std::string line_to_print = "Time out event at time [" + simTime().str() + "], at Node [" + this->getName()[4] + "] for frame with seq_num=[" + std::to_string(seqnum) + "]; \n";
     std::cout << line_to_print << std::endl;
     outputBuffer.push_back(line_to_print);
+}
+
+std::string Node::get_current_dir()
+{
+    char buff[FILENAME_MAX]; // create string buffer to hold path
+    GetCurrentDir(buff, FILENAME_MAX);
+    std::string current_working_dir(buff);
+    return current_working_dir;
+}
+void Node::readInputFile(std::string &fileName, std::vector<std::bitset<4>> *errors, std::vector<std::string> *messages)
+{
+    // Open the file
+    std::ifstream input_file;
+    input_file.open(get_current_dir() + "\\" + fileName, std::ios::in);
+    // Return if file was not opened
+    if (!input_file.is_open())
+    {
+        std::cerr << "[NODE] Error opening file." << std::endl;
+        return;
+    }
+    else
+    {
+        std::bitset<4> error;
+        std::string message;
+        while (input_file >> error)
+        {
+            std::getline(input_file, message);
+            errors->push_back(error);
+            messages->push_back(message);
+        }
+        input_file.close();
+    }
+}
+
+void Node::sendMessage(Message_Base *msg, std::bitset<4> error, const char *gateName)
+{
+
+    if ((error & std::bitset<4>(errorType::Loss)) != std::bitset<4>(0))
+    {
+        return;
+    }
+    delayTime += par("TransmissionDelay").doubleValue();
+    if ((error & std::bitset<4>(errorType::Modification)) != std::bitset<4>(0))
+    {
+        modifyMessage(msg);
+    }
+    if ((error & std::bitset<4>(errorType::Delay)) != std::bitset<4>(0))
+    {
+        delayTime += par("ErrorDelay").doubleValue();
+    }
+    if ((error & std::bitset<4>(errorType::Duplication)) != std::bitset<4>(0))
+    {
+        // duplication
+        sendDelayed(msg, delayTime, gateName);
+        Message_Base *duplicated_msg = new Message_Base(*msg);
+        delayTime += par("DuplicationDelay").doubleValue();
+        sendDelayed(duplicated_msg, delayTime, gateName);
+    }
+    else
+    {
+        sendDelayed(msg, delayTime, gateName);
+    }
 }
--- src/node.h
+++ src/node.h
@@ -20,23 +20,44 @@
 #include "message_m.h"
 #include <fstream>
 #include <iostream>
+#include <bitset>
 using namespace omnetpp;
 
 /**
  * TODO - Generated class
  */
+enum errorType
+{
+  Modification = 8,
+  Loss = 4,
+  Duplication = 2,
+  Delay = 1
+};
+
 class Node : public cSimpleModule
 {
+private:
+  std::vector<std::bitset<4>> errors;
+  std::vector<std::string> messages;
+  bool isSender;
+  int messageIndex;
+  int windowStart;
+  int windowEnd;
+  int maxSeqNo;
+
 protected:
   virtual void initialize() override;
   virtual void handleMessage(cMessage *msg) override;
   char calculateParity(std::string &payload);
   char calculateCRC(std::string &payload);
   void framing(Message_Base *mptr, std::string &payload);
-  void modifyMessage(std::string &payload);
+  void modifyMessage(Message_Base *msg);
   void openOutputFile();
   void fillOutputFile();
   void Timeout_print(int seqnum);
+  std::string get_current_dir();
+  void readInputFile(std::string &fileName, std::vector<std::bitset<4>> *errors, std::vector<std::string> *messages);
+  void sendMessage(Message_Base *msg, std::bitset<4> error, const char *gateName);
 
 public:
   // Output file for logs
